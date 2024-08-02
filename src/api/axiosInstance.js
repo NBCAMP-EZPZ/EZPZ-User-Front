7fc1a68b021b622f53df7ed0d793aa1104319c99
llocr <36-96@naver.com>
2024-08-02 15:43:58 +0900

    Refactor : env에서 API Path 가져오도록 설정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 인스턴스 생성
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
